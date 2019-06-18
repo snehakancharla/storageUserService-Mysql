d79c17d08fb78578cee635e67f741ac15825ef8a
sneha <lakshmisneha.516@gmail.com>
2019-06-18 09:40:22 +0530

    with mysql

---
+++ src/main/java/com/stackroute/storageservice/domain/User.java
@@ -0,0 +1,30 @@
+package com.stackroute.storageservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String emailId;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String password;
+    @NotNull
+    private String mobileNo;
+    @NotNull
+    private String aadharNo;
+}
---
+++ src/main/java/com/stackroute/storageservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.stackroute.storageservice.repository;
+
+import com.stackroute.storageservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+
+}
